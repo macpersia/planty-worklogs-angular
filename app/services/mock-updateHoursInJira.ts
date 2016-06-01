3c3720269b49c64c324517e05148d9b99d29ed49
pere065 <michelle.perello@bertelsmann.de>
2016-06-01 13:19:40 +0800

    added synching hours from cats to jira

---
+++ app/services/mock-updateHoursInJira.ts
@@ -0,0 +1,33 @@
+
+import { Worklog } from '../model/worklog';
+
+// let TIMESTAMP = new Date();
+// let MILLIS_A_DAY =  24 * 60 * 60 * 1000;
+// export let MOCK_WORKLOGS: Worklog[] = [{
+//   key: 'ABC-1',
+//   date: new Date(TIMESTAMP.valueOf() - MILLIS_A_DAY),
+//   duration: 0.5,
+//   description: "My Sample Worklog 1"
+// }, {
+//   key: 'ABC-2',
+//   date: new Date(TIMESTAMP.valueOf()),
+//   duration: 1.0,
+//   description: "My Sample Worklog 2"
+// }, {
+//   key: 'ABC-3',
+//   date: new Date(TIMESTAMP.valueOf() + MILLIS_A_DAY),
+//   duration: 1.5,
+//   description: "My Sample Worklog 3"
+// }, {
+//   key: 'ABC-4',
+//   date: new Date(TIMESTAMP.valueOf() + MILLIS_A_DAY * 2),
+//   duration: 2.0,
+//   description: "My Sample Worklog 4"
+// }];
+export let MOCK_UPDATED_JIRA_WORKLOG: Worklog = Worklog.fromJson({
+  key: "BICM-3186",
+  date: "2016-05-11",
+  description: "BICM-3186",
+  durationInJira:5,
+  durationInCats:5
+});
