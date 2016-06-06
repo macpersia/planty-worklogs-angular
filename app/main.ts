216d96f6dc4aed6554aad69fbc6059a00de6634c
Hadi <hadi.ahmadi@bertelsmann.de>
2016-06-06 14:24:56 +0800

    Restructured the client app to use gulp as its build system, and to provide build for production.

--- app/main.ts
+++
@@ -1,6 +0,0 @@
-
-import { bootstrap } from '@angular/platform-browser-dynamic';
-
-import { AppComponent } from './app.comp';
-
-bootstrap(AppComponent);
---
+++ src/client/app/components/config/config.comp.ts
@@ -0,0 +1,49 @@
+
+import { Component, EventEmitter, Output } from '@angular/core';
+
+import { Router } from '@angular/router-deprecated/router';
+
+import { MdCard } from '@angular2-material/card/card';
+import { MdToolbar } from '@angular2-material/toolbar/toolbar';
+import { MdInput, MdHint } from '@angular2-material/input/input';
+import { MdButton } from '@angular2-material/button/button';
+
+import { WorklogService } from '../../services/worklog.service';
+
+import { ReportParams } from '../../model/report-params';
+
+@Component({
+  selector: 'config',
+  templateUrl: 'config.comp.html',
+  // styleUrls: ['config.comp.css'],
+  directives: [
+    MdCard, MdToolbar, MdInput, MdHint, MdButton
+  ],
+  providers: [ WorklogService ]
+})
+export class ConfigComponent {
+  private reportParams = new ReportParams();
+
+  constructor(
+    private _worklogService: WorklogService,
+    private _router: Router) { }
+
+  routerOnActivate() {
+    console.log('>>>>> START worklog-list--routerOnActivate() <<<<<');
+
+    console.log(".............GETTING reportParams from sessionStorage...............");
+    let storedReportParams = this._worklogService.getReportParams();
+    console.log(storedReportParams);
+    if (storedReportParams)
+      this.reportParams = storedReportParams;
+  }
+
+  saveReportParams() {
+    console.log('>>>>> config-setReportParams <<<<<');
+    console.log(this.reportParams);
+    this._worklogService.setReportParams(this.reportParams);
+
+    //navigate to Worklogs page
+    this._router.navigate(['Worklogs']);
+  }
+}
